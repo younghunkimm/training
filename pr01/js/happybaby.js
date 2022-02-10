a2fc0df7952ee00ed2269be77ed8ea3aab63bdc8
hunifive <hunifive@gmail.com>
2022-02-10 20:49:00 +0900

    content

--- pr01/js/happybaby.js
+++ pr01/js/happybaby.js
@@ -1,18 +1,30 @@
 $(function(){
 // --------------------------------------------------
-$('.main_slider').slick({
-    arrows:false,
-    autoplay:true,
-    pauseOnHover:false,
-    pauseOnFocus:false,
-});
-
-$('.main_slider').on('afterChange', function(e,s,c){
-    // console.log(c); 슬라이더 순서의 숫자가 나온다.
-    $('.slider_dots ul li').eq(c).addClass('on').siblings().removeClass('on');
-});
 
 
+    $('.main_slider').slick({
+        arrows:false,
+        autoplay:true,
+        fade:true,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+    });
+
+    $('.main_slider').on('afterChange', function(e,s,c){
+        // console.log(c); 슬라이더 순서의 숫자가 나온다.
+        $('.slider_dots ul li').eq(c).addClass('on').siblings().removeClass('on');
+    });
+
+    $(window).on('scroll', function(){
+        var sct = $(window).scrollTop();
+        // console.log(sct);
+
+        if (sct > 100) {
+            $('#header').addClass('on');
+        } else {
+            $('#header').removeClass('on');
+        }
+    });
 
 
 
