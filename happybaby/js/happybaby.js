2db71fe0e9cf23f8e0d3e367eb3e361bc945cdff
hunifive <hunifive@gmail.com>
2022-03-27 23:49:28 +0900

    220327

---
+++ happybaby/js/happybaby.js
@@ -0,0 +1,39 @@
+$(function(){
+// --------------------------------------------------
+
+
+    $('.main_slider').slick({
+        arrows:false,
+        autoplay:true,
+        fade:true,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+    });
+
+    $('.main_slider').on('afterChange', function(e,s,c){
+        // console.log(c); 슬라이더 순서의 숫자가 나온다.
+        $('.slider_dots ul li').eq(c).addClass('on').siblings().removeClass('on');
+    });
+
+    $(window).on('scroll', function(){
+        var sct = $(window).scrollTop();
+        // console.log(sct);
+
+        if (sct > 100) {
+            $('#header').addClass('on');
+        } else {
+            $('#header').removeClass('on');
+        }
+    });
+
+    $('#header .menu_open_btn').on('click', function(){
+        $(this).toggleClass('on');
+        $('#header .gnb_wrap').toggleClass('on');
+    });
+
+    const topLine = $('#header .top_line').clone();
+
+    $('#header .gnb_wrap nav').prepend(topLine);
+
+// --------------------------------------------------
+})
